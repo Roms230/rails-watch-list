bb6c801452f00fd3241220cc107ae24281ff1c5a
Romain Thiboudois <romainthiboudois@gmail.com>
2022-10-07 11:17:32 +0400

    update

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+  def destroy
+    # @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.destroy
+      redirect_to list_path(@bookmark.list)
+    else
+      render list_path(@bookmark.list), notice: "Something went wrong!"
+    end
+  end
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
